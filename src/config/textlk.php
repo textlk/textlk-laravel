e5fb76a7fb8959bd8a303331e427b4db710f8a61
chamaraindrajith <chamara.indrajith@outlook.com>
2024-02-11 23:16:58 +0530

    services.php

--- src/config/textlk.php
+++ src/config/textlk.php
@@ -11,23 +11,7 @@
  */
 
 return [
-    'nexmo_sms_api_settings' => [
-        'API_KEY' => env('NEXMO_API_KEY', ''),
-        'API_SECRET' => env('NEXMO_API_SECRET', ''),
-        'SEND_SMS_FROM' => env('NEXMO_SMS_FROM', 'IYNGARAN'),
-    ],
-    'twilio_sms_api_settings' => [
-        'SID' => env('TWILIO_SID', ''),
-        'TOKEN' => env('TWILIO_TOKEN', ''),
-        'SEND_SMS_FROM' => env('TWILIO_SMS_FROM', '+12012926824'),
-    ],
-    'message_bird_sms_api_settings' => [
-        'API_KEY' => env('MESSAGE_BIRD_API_KEY', ''),
-        'SEND_SMS_FROM' => env('MESSAGE_BIRD_SMS_FROM', '+12012926824'),
-    ],
-    'dialog_sms_api_settings' => [
-        'API_KEY' => env('DIALOG_SMS_API_KEY', ''),
-        'ENDPOINT' => env('DIALOG_SMS_ENDPOINT', ''),
-        'SEND_SMS_FROM' => env('DIALOG_SMS_FROM', 'IYNGARAN'),
+    'textlk' => [
+        'key' => env('TEXTLK_API_KEY'),
     ],
 ];
