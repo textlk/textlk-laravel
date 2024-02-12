dd19189f0f70ee5026439e817668c863e7286536
chamaraindrajith <chamara.indrajith@outlook.com>
2024-02-12 10:37:37 +0530

    test gateway

--- src/Notifications/Channels/TextlkChannel.php
+++ src/Notifications/Channels/TextlkChannel.php
@@ -4,12 +4,28 @@ namespace Textlk\Notifications\Channels;
 
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
+use Textlk\SMS;
 
 class TextlkChannel
 {
     public function send($notifiable, Notification $notification)
     {
         // Logic to send the notification via Textlk
-        Log::error("Logic to send the notification via Textlk TextlkChannel");
+        Log::error("Logic to send the notification via Textlk TextlkChannel. Modified.");
+        $api_key = '2|3SwUUnFUfqJm09VcxCPnHeYgY5mV4LkJQdyHTISN29f88d3e';
+
+        // Instantiate Textlk\SMS with the required arguments
+        $SMS = new SMS($api_key);
+
+        $data = array(
+            "recipient" => "94764880118", // or have multiple numbers: "recipient" => "+9476000000,+9476111000"
+            "sender_id" => "TEXTLK",
+            "type" => "plain",
+            "message" => "Boom! Message from Text.lk"
+            // "schedule_time" => "2021-12-20T07:00:00Z"
+        );
+
+        $response = $SMS->send($data);
+        Log::error($response);
     }
 } 
